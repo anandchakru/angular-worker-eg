1b53c9e020b517ee1cd88b038bd4d178de070b16
Anand <anandchakru.forum@gmail.com>
2019-12-26 08:52:10 -0800

    buildinfo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BUILDINFO } from '../environments/meta';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  bi = BUILDINFO;
   DATA = 'Message to be hashed';
   hashd;
   private worker: Worker;
